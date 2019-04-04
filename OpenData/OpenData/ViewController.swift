52ac7360171600dd43c814010b1a256bb726b699
xujiyou <xujiyou555@gmail.com>
2019-04-04 20:08:43 +0800

    one

---
+++ OpenData/OpenData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenData
+//
+//  Created by jiyou xu on 2019/4/4.
+//  Copyright © 2019 jiyou xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
